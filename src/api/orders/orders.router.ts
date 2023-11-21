0fd7ad5306930ef2994625ca2a421e3b106dde45
zevik <zevik2001@gmail.com>
2023-11-21 16:17:28 +0200

    create orders routs

--- src/api/orders/orders.router.ts
+++ src/api/orders/orders.router.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { handleGetOrders } from "./orders.controller";
-
+import { GetOrdersById } from "./orders.controller";
+import { postOrder } from "./orders.controller";
+import { putOrder } from "./orders.controller";
 const router = express.Router();
 
 router.get("/", handleGetOrders);
-
+router.get("/:id",GetOrdersById);
+router.post("/",postOrder);
+router.put("/:id",putOrder);
 export default router;
--- src/api/orders/orders.service.ts
+++ src/api/orders/orders.service.ts
@@ -1,14 +1,51 @@
-import * as ordersDal from './orders.dal'
+import * as ordersDal from "./orders.dal";
 import chalk from "chalk";
-
-
-
+import { OrderInterface } from "./ordersInterface";
 
 export const getOrders = async () => {
   try {
     const orders = await ordersDal.getOrders();
     if (!orders) throw new Error("no products in the database");
     return orders;
+  } catch (error) {
+    console.log(chalk.redBright(error));
+    return Promise.reject(error);
+  }
+};
+
+export const GetOrdersById = async (id: string) => {
+  try {
+    const orders = await ordersDal.GetOrdersById(id);
+    if (!orders)
+      throw new Error("no found products with current id in the database");
+    return orders;
+  } catch (error) {
+    console.log(chalk.redBright(error));
+    return Promise.reject(error);
+  }
+};
+
+export const postOrder = async (order: OrderInterface) => {
+  try {
+    if (order) {
+      const orders = await ordersDal.postOrder(order);
+      return orders;
+    } else {
+      throw new Error("cant add order");
+    }
+  } catch (error) {
+    console.log(chalk.redBright(error));
+    return Promise.reject(error);
+  }
+};
+export const putOrder = async (id:string,order: OrderInterface) => {
+  try {
+    if (order) {
+      const orders = await ordersDal.putOrder(id,order);
+      return orders;
+    } else {
+      throw new Error("cant add order");
+    }
   } catch (error) {
     console.log(chalk.redBright(error));
     return Promise.reject(error);
