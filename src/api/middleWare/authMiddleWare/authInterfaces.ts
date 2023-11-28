50d6836f776912bcc16aa41544e93145249134e1
mendi <mendi3266@gmail.com>
2023-11-28 08:30:36 +0200

    scan and fix code of middlewraes

---
+++ src/api/middleWare/authMiddleWare/authInterfaces.ts
@@ -0,0 +1,10 @@
+import { Request } from "express";
+export interface tokenDataInterface{
+    email: string;
+    isAdmin: boolean;
+}
+
+export interface UserRequest extends Request {
+    user?: Partial<tokenDataInterface>;
+  }
+
